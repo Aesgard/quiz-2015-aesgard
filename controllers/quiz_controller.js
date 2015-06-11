a063a484c3a727f4f3f712d956af777dc5b9de3b
Aesgard <aesgard.software.lab@gmail.com>
2015-06-11 12:18:02 +0200

    Validacion entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -27,7 +27,7 @@ exports.index = function(req, res) {
 	};
 	if(search.length>0) search = 'Preguntas que continene: "'+search+'"';
 	models.Quiz.findAll(options).then(function(quizes) {
-		res.render('quizes/index.ejs', {quizes: quizes, search: search});
+		res.render('quizes/index.ejs', {quizes: quizes, search: search, errors: []});
 	});
 }
 
@@ -36,23 +36,29 @@ exports.new = function(req, res) {
 	var quiz = models.Quiz.build( // crea objeto quiz
 		{pregunta: "Pregunta", respuesta: "Respuesta"}
 	);
-	res.render('quizes/new', {quiz: quiz});
+	res.render('quizes/new', {quiz: quiz, errors: []});
 }
 
 // POST /quizes/create
 exports.create = function(req, res) {
 	var quiz = models.Quiz.build( req.body.quiz );
 	
-	//Guarda en DB los campos pregunta y respuesta de quiz
-	quiz.save({fields: ["pregunta", "respuesta"]}).then(function() {
-		res.redirect('/quizes');
-	}); //Redireccion HTTP (URL relativo) lista de preguntas
+	quiz.validate().then(function(err) {
+		if(err) {
+			res.render('quizes/new', {quiz: quiz, errors: err.errors});
+		} else {
+			//Guarda en DB los campos pregunta y respuesta de quiz
+			quiz.save({fields: ["pregunta", "respuesta"]}).then(function() {
+				res.redirect('/quizes');
+			}); //Redireccion HTTP (URL relativo) lista de preguntas
+		}
+	});
 }
 
 // GET /quizes/:id
 exports.show = function(req, res) {
 	models.Quiz.findById(req.params.quizId).then(function(quiz) {
-		res.render('quizes/show', {quiz:req.quiz});
+		res.render('quizes/show', {quiz:req.quiz, errors: []});
 	});
 }
 
@@ -62,11 +68,11 @@ exports.answer = function(req, res) {
 		var resultado = 'Incorrecto';
 		if (req.query.respuesta === req.quiz.respuesta)
 			resultado = 'Correcto';
-		res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado });
+		res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado, errors: [] });
 	});
 };
 
 //GET /author
 exports.author = function(req, res) {
-	res.render('author');
+	res.render('author', {errors: []});
 };
