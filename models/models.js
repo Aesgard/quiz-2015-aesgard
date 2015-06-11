4fb513b4219f22ddc4f3bec87c7a6dadb9e3337e
Aesgard <aesgard.software.lab@gmail.com>
2015-06-11 19:13:25 +0200

    Inicializar tabla nuevo campo

--- models/models.js
+++ models/models.js
@@ -32,7 +32,7 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz = Quiz; // exportar definicion de la tabla Quiz
 
 // sequelize.sync() crea e inicia tabla de preguntas en DB
-sequelize.sync().then(function() {
+sequelize.sync({force: true}).then(function() {
 	//success(...) ejecuta el manejador una vez creada la tabla.
 	Quiz.count().then(function(count) {
 		if(count === 0) { //la tabla se inicializa solo i esta vacia
